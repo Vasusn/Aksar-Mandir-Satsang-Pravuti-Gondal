05cd3ce1437741b21798e69dca14c22a941e846c
Vasusn <vasunageshri@gmail.com>
2024-10-05 12:46:17 +0530

    check when user is loged in or not

---
+++ lib/Screens/initial_screen.dart
@@ -0,0 +1,34 @@
+import 'package:aksar_mandir_gondal/Screens/home_screen.dart';
+import 'package:aksar_mandir_gondal/Screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class InitialScreen extends StatelessWidget {
+  const InitialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // If the user is logged in, navigate to HomeScreen
+        if (snapshot.connectionState == ConnectionState.active) {
+          final user = snapshot.data;
+          if (user != null) {
+            return const HomeScreen();
+          } else {
+            // If the user is not logged in, navigate to LoginScreen
+            return const LoginScreen();
+          }
+        } else {
+          // While checking the authentication state, show a loading spinner
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
