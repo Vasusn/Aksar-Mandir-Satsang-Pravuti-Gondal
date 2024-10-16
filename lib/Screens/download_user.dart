5edc703fdf6eedbc79086a072d4ee52f85e48faa
Vasusn <vasunageshri@gmail.com>
2024-10-16 11:30:04 +0530

    fix some error

--- lib/Screens/download_user.dart
+++ lib/Screens/download_user.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
